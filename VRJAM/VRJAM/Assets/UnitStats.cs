616972c99a7b2d224c3ec92637d83a89d593130f
Hahoolah <oskar@kylvag.com>
2016-11-12 10:31:27 +0100

    drty7okldtf

---
+++ VRJAM/VRJAM/Assets/UnitStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitStats : MonoBehaviour
+{
+    int myUnitID;
+    int myHealth;
+    int mySpeed;
+    int myDamage;
+    bool myIsRanged;
+
+    void start()
+    {
+        myUnitID = 1;
+        myHealth = 1;
+        mySpeed = 1;
+        myDamage = 1;
+        myIsRanged = false;
+    }
+
+    void update()
+    {
+
+    }
+}
