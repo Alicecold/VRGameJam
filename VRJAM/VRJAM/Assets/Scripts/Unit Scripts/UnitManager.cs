db795ec010e2638f6047c1479b8d4453d921ccf4
Marcus <marcus.goransson.sp14@thegameassembly.com>
2016-11-12 21:31:17 +0100

    group stuff

--- VRJAM/VRJAM/Assets/Scripts/Unit Scripts/UnitManager.cs
+++ VRJAM/VRJAM/Assets/Scripts/Unit Scripts/UnitManager.cs
@@ -13,11 +13,12 @@ public class UnitManager : MonoBehaviour
     private bool myTeam; // True = white, false = black
     private bool myIsMoving;
     private Vector3 myDestination;
+    private GameObject myTarget;
 
     void Start()
     {
         GroupManager InitSettings = transform.parent.gameObject.GetComponent<GroupManager>();
-        myMovementSpeed = InitSettings.myMovementSpeed;
+        myMovementSpeed = InitSettings.myMovementSpeed - 0.01f;
         myDamping = InitSettings.myDamping;
         myUnitID = InitSettings.myUnitID;
         myHealth = InitSettings.myHealth;
@@ -25,22 +26,26 @@ public class UnitManager : MonoBehaviour
         myAttackRange = InitSettings.myAttackRange;
 
         myTeam = InitSettings.myTeam;
+        myTarget = InitSettings.myTarget;
       }
 
     void Update()
     {
-        GameObject myParent = transform.parent.gameObject;
-        myDestination = myParent.transform.position;
+       
+        myDestination = myTarget.transform.localToWorldMatrix.GetPosition();
         myDestination.y = 0;
-        if ((myDestination - transform.position).magnitude < .3)
+        Vector3 position = transform.position;
+        position.y = 0;
+
+        if ((myDestination - position).magnitude < 1.0)
         {
             myIsMoving = false;
         }
         else
         {
-            Vector3 direction = Vector3.Lerp(transform.position, myDestination, Time.fixedDeltaTime * myMovementSpeed);
+            Vector3 direction = Vector3.Lerp(position, myDestination, Time.fixedDeltaTime * myMovementSpeed);
             transform.position = direction;
-            Vector3 lookPos = myDestination - transform.position;
+            Vector3 lookPos = myDestination - position;
             lookPos.y = 0;
             Quaternion rotation = Quaternion.LookRotation(lookPos);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * myDamping);
