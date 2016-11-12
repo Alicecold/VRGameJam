e36a98813190d538398d74c38932185d8aaef273
Marcus <marcus.goransson.sp14@thegameassembly.com>
2016-11-12 10:27:19 +0100

    Added vr settings script

---
+++ VRJAM/VRJAM/Assets/VRSettingsScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class VRSettingsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //If V is pressed, toggle VRSettings.enabled
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            VRSettings.enabled = !VRSettings.enabled;
+            Debug.Log("Changed VRSettings.enabled to:" + VRSettings.enabled);
+        }
+    }
+}
