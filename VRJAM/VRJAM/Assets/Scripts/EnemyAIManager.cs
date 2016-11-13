02fa7284d75a71980664774a63cbb46e1b79e599
Hahoolah <oskar@kylvag.com>
2016-11-13 15:00:15 +0100

    Modified AI

--- VRJAM/VRJAM/Assets/Scripts/EnemyAIManager.cs
+++ VRJAM/VRJAM/Assets/Scripts/EnemyAIManager.cs
@@ -8,7 +8,7 @@ public class EnemyAIManager : MonoBehaviour {
     public GameObject myArcherTarget;
     Vector3 myPawnPosition;
     Vector3 myKnightPosition;
-    Vector3 myArcherPosition
+    Vector3 myArcherPosition;
     public float myCountDown;
 
 	// Use this for initialization
@@ -21,12 +21,12 @@ public class EnemyAIManager : MonoBehaviour {
         myCountDown -= Time.deltaTime;
         if (myCountDown <= 10)
         {
-            myPawnPosition = new Vector3(Random.Range(-20.0f, 20.0f), 0, Random.Range(-20.0f, 20.0f));
+            myPawnPosition = new Vector3(Random.Range(-1000.0f, 2500.0f), 254, Random.Range(-200.0f, -2600.0f));
             myPawnTarget.transform.position = myPawnPosition;
-            myKnightPosition = new Vector3(Random.Range(-20.0f, 20.0f), 0, Random.Range(-20.0f, 20.0f));
+            myKnightPosition = new Vector3(Random.Range(-1000.0f, 2500.0f), 254, Random.Range(-200.0f, -2600.0f));
             myKnightTarget.transform.position = myKnightPosition;
-            myArcherPosition = new Vector3(Random.Range(-20.0f, 20.0f), 0, Random.Range(-20.0f, 20.0f));
-            myArcherTarget.transform.position = myKnightPosition;
+            myArcherPosition = new Vector3(Random.Range(-1000.0f, 2500.0f), 254, Random.Range(-200.0f, -2600.0f));
+            myArcherTarget.transform.position = myArcherPosition;
             myCountDown = 15f;
         }
 	}
