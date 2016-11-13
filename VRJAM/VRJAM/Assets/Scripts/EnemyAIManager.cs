03ff6d92fd4e9e23ff0670604d458badade13479
Marcus <marcus.goransson.sp14@thegameassembly.com>
2016-11-13 17:03:43 +0100

    FINAL JAM COMMIT

--- VRJAM/VRJAM/Assets/Scripts/EnemyAIManager.cs
+++ VRJAM/VRJAM/Assets/Scripts/EnemyAIManager.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyAIManager : MonoBehaviour {
+public class EnemyAIManager : MonoBehaviour
+{
 
     public GameObject myPawnTarget;
     public GameObject myKnightTarget;
@@ -11,27 +12,40 @@ public class EnemyAIManager : MonoBehaviour {
     Vector3 myArcherPosition;
     public float myCountDown;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         myCountDown -= Time.deltaTime;
         if (myCountDown <= 10)
         {
-            myPawnTarget.GetComponent<Rigidbody>().AddForce(transform.forward * 4000.0f);
-            myKnightTarget.GetComponent<Rigidbody>().AddForce(transform.forward * 4000.0f);
-            myArcherTarget.GetComponent<Rigidbody>().AddForce(transform.forward * 4000.0f);
+            if (myPawnTarget != null)
+            {
+                myPawnTarget.GetComponent<Rigidbody>().AddForce(transform.forward * 4000.0f);
+            }
 
+            if (myKnightTarget != null)
+            {
+                myKnightTarget.GetComponent<Rigidbody>().AddForce(transform.forward * 4000.0f);
+            }
+
+            if (myArcherTarget != null)
+            {
+                myArcherTarget.GetComponent<Rigidbody>().AddForce(transform.forward * 4000.0f);
+            }
+            
             //myPawnPosition = new Vector3(Random.Range(-20.0f, 20.0f), 0, Random.Range(-20.0f, 20.0f));
             //myPawnTarget.transform.position = myPawnPosition;
             // myKnightPosition = new Vector3(Random.Range(-20.0f, 20.0f), 0, Random.Range(-20.0f, 20.0f));
             //myKnightTarget.transform.position = myKnightPosition;
             //myArcherPosition = new Vector3(Random.Range(-20.0f, 20.0f), 0, Random.Range(-20.0f, 20.0f));
             //myArcherTarget.transform.position = myKnightPosition;
-            myCountDown = 11f;
+            myCountDown = 18f;
         }
-	}
+    }
 }
